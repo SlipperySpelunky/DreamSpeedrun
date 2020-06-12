cd22eae87bd0b1bc3be9c462eec75a031c1645e4
SlipperySpelunky <gerogarcia29@gmail.com>
2020-06-12 17:40:23 -0500

    yes

--- src/main/java/me/slipperyspelunky/dreamspeedrun/listeners/ItemRightClick.java
+++ src/main/java/me/slipperyspelunky/dreamspeedrun/listeners/ItemRightClick.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class ItemRightClick implements Listener {
     private final Methods methods = new Methods();
@@ -21,20 +20,10 @@ public class ItemRightClick implements Listener {
 
         if(target == null) target = player;
 
-        ItemStack whatToGet = player.getInventory().getItemInHand();
-        if(Bukkit.getVersion().contains("1.8"))
-
         if(!(methods.getConfigString("speedrunner").equals(player.getName()))) {
-            if(!(Bukkit.getVersion().contains("1.8"))) {
-                if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && (player.getInventory().getItemInMainHand().getType().equals(Material.COMPASS) || player.getInventory().getItemInOffHand().getType().equals(Material.COMPASS))) {
-                    player.setCompassTarget(target.getLocation());
-                    player.sendMessage(DreamSpeedrun.getPrefix() + methods.translateColor("&aCompass is now pointing to &e" + target.getName() + "&a."));
-                }
-            }else{
-                if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getInventory().getIte) {
-                    player.setCompassTarget(target.getLocation());
-                    player.sendMessage(DreamSpeedrun.getPrefix() + methods.translateColor("&aCompass is now pointing to &e" + target.getName() + "&a."));
-                }
+            if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && (player.getInventory().getItemInMainHand().getType().equals(Material.COMPASS) || player.getInventory().getItemInOffHand().getType().equals(Material.COMPASS))) {
+                player.setCompassTarget(target.getLocation());
+                player.sendMessage(DreamSpeedrun.getPrefix() + methods.translateColor("&aCompass is now pointing to &e" + target.getName() + "&a."));
             }
         }
     }
